ac3497198dcf2c93230a8d5e124190b54f3ccee2
Julian Perez <julianjp@outlook.com>
2019-02-02 16:59:53 -0500

    subiendo archivos

---
+++ precache-manifest.19f7eb488d2ce8994deec6f6ad80e435.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a7b78b8ae762ea86293",
+    "url": "/parkingapp-react/static/js/runtime~main.5a7b78b8.js"
+  },
+  {
+    "revision": "82b21d906829f5b3754a",
+    "url": "/parkingapp-react/static/js/main.82b21d90.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/parkingapp-react/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "82b21d906829f5b3754a",
+    "url": "/parkingapp-react/static/css/main.459f2e40.chunk.css"
+  },
+  {
+    "revision": "9177de801f5fe9d35446769154fea5b5",
+    "url": "/parkingapp-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/parkingapp-react/precache-manifest.19f7eb488d2ce8994deec6f6ad80e435.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/parkingapp-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
